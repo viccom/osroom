50483d6bce9858722daa520bc3311e5cff43134a
HiWoo <xiaopingwoo@163.com>
2018-05-13 02:41:33 +0800

    更新配置

--- apps/configs/db_config_sample.py
+++ apps/configs/db_config_sample.py
@@ -2,50 +2,50 @@
 __author__ = "Allen Woo"
 DB_CONFIG = {
     "redis": {
+        "password": "<Your password>",
         "host": [
             "127.0.0.1"
         ],
-        "password": "<Your password>",
         "port": [
             "6379"
         ]
     },
     "mongodb": {
         "mongo_web": {
-            "username": "work",
-            "host": [
-                "127.0.0.1:27017"
-            ],
             "password": "<Your password>",
-            "dbname": "osr_web",
+            "username": "work",
             "config": {
                 "fsync": False,
                 "replica_set": None
-            }
-        },
-        "mongo_sys": {
-            "username": "work",
+            },
             "host": [
                 "127.0.0.1:27017"
             ],
-            "password": "<Your password>",
-            "dbname": "osr_sys",
-            "config": {
-                "fsync": False,
-                "replica_set": None
-            }
+            "dbname": "osr_web"
         },
         "mongo_user": {
-            "username": "work",
-            "host": [
-                "127.0.0.1:27017"
-            ],
             "password": "<Your password>",
-            "dbname": "osr_user",
+            "username": "work",
             "config": {
                 "fsync": False,
                 "replica_set": None
-            }
+            },
+            "host": [
+                "127.0.0.1:27017"
+            ],
+            "dbname": "osr_user"
+        },
+        "mongo_sys": {
+            "password": "<Your password>",
+            "username": "work",
+            "config": {
+                "fsync": False,
+                "replica_set": None
+            },
+            "host": [
+                "127.0.0.1:27017"
+            ],
+            "dbname": "osr_sys"
         }
     }
 }
